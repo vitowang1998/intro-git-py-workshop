72b8c58c85959b2b9394aa69663c195186fcbd2d
Haoran Wang <vitowang1998@gmail.com>
2018-03-15 20:04:46 -0400

    Adds the design recipe for two functions.

--- caduceus/sorter.py
+++ caduceus/sorter.py
@@ -3,8 +3,28 @@ class SnakeSorter:
         self.snakes = snakes
         self.sorted_snakes = []
 
+    '''
+    sort_by_weight sorts the snakes by their weight in ascending order.
+
+    Args:
+        self: A SnakeSorter object.
+
+    Returns:
+        Function mutates the sorted_sankes of the self object and returns
+        nothing.
+    '''
     def sort_by_weight(self):
         self.sorted_snakes = sorted(self.snakes, key=lambda snek: snek.weight)
 
+    '''
+    sort_by_length sorts the snakes by their length in ascending order.
+
+    Args:
+        self: A SnakeSorter object.
+
+    Returns:
+        Function mutates the sorted_sankes part of the self object and returns
+        nothing.
+    '''
     def sort_by_length(self):
         self.sorted_snakes = sorted(self.snakes, key=lambda snek: snek.length)
